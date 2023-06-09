891802cc777d71ee84e2b07ee267b49ec380c261
Javier del Canto <javernaver@gmail.com>
2023-06-09 15:54:16 -0400

    counter bloc

---
+++ lib/presentation/blocs/bloc/counter_state.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_bloc.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+  final int transactionCount;
+
+  const CounterState({
+    this.counter = 10,
+    this.transactionCount = 0,
+  });
+
+  CounterState copyWith({
+    int? counter,
+    int? transactionCount,
+  }) =>
+      CounterState(
+        counter: counter ?? this.counter,
+        transactionCount: transactionCount ?? this.transactionCount,
+      );
+
+  @override
+  List<Object> get props => [counter, transactionCount];
+}
