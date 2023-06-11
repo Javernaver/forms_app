3fcf6e9748ad58554ec2fbf99c1f8d8f61bfa082
Javier del Canto <javernaver@gmail.com>
2023-06-11 19:47:34 -0400

    forms with formz

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -48,30 +48,23 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
-
-  @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final password = registerCubit.state.password;
+    final email = registerCubit.state.email;
 
     return Form(
-      key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Nombre de usuario',
-            onChanged: (value) {
-              registerCubit.usernameChanged(value);
-              _formKey.currentState?.validate();
-            },
+            onChanged: registerCubit.usernameChanged,
+            errorMessage: username.errorMessage,
             prefixIcon: Icons.person,
             validator: (value) {
               if (value == null || value.isEmpty || value.trim().isEmpty) {
@@ -83,31 +76,27 @@ class _RegisterFormState extends State<_RegisterForm> {
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'Correo electronico',
-            onChanged: (value) {
-              registerCubit.emailChanged(value);
-              _formKey.currentState?.validate();
-            },
+            onChanged: registerCubit.emailChanged,
+            errorMessage: email.errorMessage,
             prefixIcon: Icons.email_outlined,
-            validator: (value) {
-              if (value == null || value.isEmpty || value.trim().isEmpty) {
-                return 'El nombre de usuario es requerido';
-              }
-              final emailRegExp = RegExp(
-                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-              );
-              if (!emailRegExp.hasMatch(value)) {
-                return 'El correo electronico no es valido';
-              }
-              return null;
-            },
+            // validator: (value) {
+            //   if (value == null || value.isEmpty || value.trim().isEmpty) {
+            //     return 'El nombre de usuario es requerido';
+            //   }
+            //   final emailRegExp = RegExp(
+            //     r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+            //   );
+            //   if (!emailRegExp.hasMatch(value)) {
+            //     return 'El correo electronico no es valido';
+            //   }
+            //   return null;
+            // },
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'Contraseña',
-            onChanged: (value) {
-              registerCubit.passwordChanged(value);
-              _formKey.currentState?.validate();
-            },
+            onChanged: registerCubit.passwordChanged,
+            errorMessage: password.errorMessage,
             prefixIcon: Icons.password,
             obscureText: true,
             // suffixIcon: GestureDetector(
@@ -124,9 +113,6 @@ class _RegisterFormState extends State<_RegisterForm> {
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
             onPressed: () {
-              final isValid = _formKey.currentState?.validate() ?? false;
-              if (!isValid) return;
-
               registerCubit.onSubmit();
             },
             icon: const Icon(Icons.save),
